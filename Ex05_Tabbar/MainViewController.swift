674c9b7c283e9fb9405489058785c9246231b084
SongYunGeun <rjawoakstp@naver.com>
2022-01-02 16:22:59 +0900

    마지 연습( 학습은 완료)

--- Ex05_Tabbar/MainViewController.swift
+++ Ex05_Tabbar/MainViewController.swift
@@ -22,6 +22,8 @@ class MainViewController: UIViewController {
         if let index = self.tabBarController?.selectedIndex{
         print("현재 탭바",index)
         }
+        
+        print("마지 연습")
         //didload와 did appear값이 다른 경우는 이제 뷰가 정렬되는 시간이 들어서
         // 생명주기로 따지면 appear이 더 뒤다.
     }
